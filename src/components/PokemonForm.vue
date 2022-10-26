b1e3e43009f84390fddb93a156c85c8208d74b47
Camilo Salamanca <casc6798-@hotmail.com>
2022-10-25 21:35:17 -0500

    1ra clase de Vue, aprendiendo sobre componentes

---
+++ src/components/PokemonForm.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="row">
+        <div class="col">
+            <form>
+                <div class="mb-3">
+                    <div id="emailHelp" class="form-text">
+                        <h1>Adivina el pokemon!</h1>
+                    </div>
+                    <label for="exampleInput" class="form-label">Pokemon</label>
+                    <input type="text" class="form-control" id="pokemonInput" aria-describedby="emailHelp">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInput1" class="form-label">Url</label>
+                    <input type="text" class="form-control" id="urlInput">
+                </div>
+                <button type="button" class="btn btn-primary" id="bringThePokemon_btn">Bring the Pokemon</button>
+                <button type="button" class="btn btn-primary" id="send_btn">Send</button>
+                <button type="button" class="btn btn-primary" id="bringmyapi_btn">Trae mi API</button>
+            </form>
+        </div>
+        <div class="col">
+            <div class="accordion" id="accordionPanelsStayOpenExample">
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="panelsStayOpen-headingOne">
+                        <button class="accordion-button" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true"
+                            aria-controls="panelsStayOpen-collapseOne">
+                            Imagen Pokemon Frontal
+                        </button>
+                    </h2>
+                    <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show"
+                        aria-labelledby="panelsStayOpen-headingOne">
+                        <div class="accordion-body">
+                            <div class="container">
+                                <div class="card" style="width: 18rem;">
+                                    <img class="card-img-top" id="imgPokemonFrontal">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="panelsStayOpen-headingThree">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false"
+                            aria-controls="panelsStayOpen-collapseThree">
+                            Imagen Pokemon Trasera
+                        </button>
+                    </h2>
+                    <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse"
+                        aria-labelledby="panelsStayOpen-headingThree">
+                        <div class="accordion-body">
+                            <div class="container">
+                                <div class="card" style="width: 18rem;">
+                                    <img class="card-img-top" id="imgPokemonTrasera">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ src/components/TablaPokemones.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="row">
+        <div class="col-12">
+            <table class="table table-dark table-striped" style="max-width: 50%; margin-left: 50%, 0%">
+                <thead>
+                    <tr>
+                        <th colspan="6">Pokemones API</th>
+                    </tr>
+                    <tr>
+                        <th>Pokemon</th>
+                        <th>Url</th>
+                    </tr>
+                </thead>
+
+                <tbody id="id_tbody">
+                    <tr>
+                        <td>Snorlax</td>
+                        <td>http://believe.in.youself.com/</td>
+                    </tr>
+                </tbody>
+
+                <tbody>
+                    <tr>
+                        <td>Pikachu</td>
+                        <td>htts23://sometimeago.com.co/s/</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</template>
+
+<style>
+
+</style>
